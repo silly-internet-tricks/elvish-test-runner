4fdaa4a49c2c725964ee5d703363422b62217873
Josh Parker <JoshWolfParker@gmail.com>
2025-07-25 17:35:31 -0600

    add the run.sh file

---
+++ bin/run.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+# Synopsis:
+# Run the test runner on a solution.
+
+# Arguments:
+# $1: exercise slug
+# $2: path to solution folder
+# $3: path to output directory
+
+# Output:
+# Writes the test results to a results.json file in the passed-in output directory.
+# The test results are formatted according to the specifications at https://github.com/exercism/docs/blob/main/building/tooling/test-runners/interface.md
+
+# Example:
+# ./bin/run.sh two-fer path/to/solution/folder/ path/to/output/directory/
+
+# If any required arguments is missing, print the usage and exit
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "usage: ./bin/run.sh exercise-slug path/to/solution/folder/ path/to/output/directory/"
+    exit 1
+fi
+
+slug="$1"
+
+solution_dir=$(realpath "${2%/}")
+output_dir=$(realpath "${3%/}")
+results_file="${output_dir}/results.json"
+
+# Create the output directory if it doesn't exist
+mkdir -p "${output_dir}"
+
+echo "${slug}: testing..."
+
+cp "$solution_dir/$slug.test.elv" ./bin/test.elv && cp "$solution_dir/$slug.elv" ./bin/solution.elv && elvish ./bin/run.elv > "$results_file"
+
+cp ./tests/example-syntax-error/MiniBob.test.elv ./bin/test.elv && cp ./tests/example-syntax-error/MiniBob.elv ./bin/solution.elv && elvish ./bin/run.elv
+
+echo "${slug}: done"
